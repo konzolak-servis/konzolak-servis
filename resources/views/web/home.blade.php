327f99e88896b4c52c9ebce72bb8884d8635c8b9
Zdeněk Gřešek <zdenek.gresek@gmail.com>
2026-09-08 13:18:21 +0200

    Web: odstraněn nadbytečný eyebrow řádek v hero

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -6,7 +6,6 @@
     <div class="hero__glow"></div>
     <img class="hero__logo" src="/images/konzolak-logo-print.png" alt="" aria-hidden="true">
     <div class="wrap hero__in">
-        <p class="eyebrow">Zlín · PlayStation · Xbox · Nintendo · PC</p>
         <h1>Servis herních konzolí a <span class="accent">PC ve Zlíně</span></h1>
         <p>Opravím PlayStation, Xbox, Nintendo i počítače. Diagnostika zdarma při opravě,
             cenu vždy odsouhlasíte předem. Osobní přístup jednoho technika.</p>
